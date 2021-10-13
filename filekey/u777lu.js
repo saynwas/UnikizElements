0001c9c04e5627b340ae9c484a971d64a4baea1f
sayandweep <81100142+sayandweep@users.noreply.github.com>
2021-10-13 18:07:12 +0530

    Create u777lu.js

---
+++ filekey/u777lu.js
@@ -0,0 +1,58 @@
+var style = document.createElement('style');
+			style.innerHTML = `
+	 .unikiz_LuBody {
+			margin: 0;
+			padding: 10px
+		}
+		.unikiz_LuSecBody {
+			display: grid;
+			grid-template-columns: auto auto
+		}
+		.unikiz_LuSingle {
+			padding: 10px
+		}
+		.unikiz_LuImgSec {
+			overflow: hidden;
+		}
+		.unikiz_LuImg {
+			-webkit-transition: .4s;
+			-moz-transition: .4s;
+			-ms-transition: .4s;
+			-o-transition: .4s;
+			transition: .4s;
+			overflow: hidden;
+			transform: scale(1)
+		}
+		.unikiz_LuImg:hover {
+			transform: scale(1.1)
+		}
+		.unikiz_LuDesSec {
+			margin-top: 16px
+		}
+		.unikiz_LuFirsDes {
+			font-size: 1.5em
+		}
+		.unikiz_LuSecoDes {
+			margin-top: -5px
+		}
+		.unikiz_LuSecoDes p {
+			display: inline-block;
+			margin-right: 4px;
+			color: #4015A9;
+			font-weight: 400;
+			font-size: 0.7em;
+			margin-bottom: -35px
+		}
+		a,a:hover {
+			color: inherit;
+			text-decoration: none
+		}
+		p {
+			line-height: 1.4em
+		}
+	`;
+			document.head.appendChild(style);
+		};
+if (1 > 2) {
+		document.getElementById("u777lu").style.display = "none";
+	};
