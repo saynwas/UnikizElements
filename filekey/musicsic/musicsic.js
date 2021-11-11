a89ccbc4697c0c03f203b677e21d29f1bfff0d74
sayandweep <81100142+sayandweep@users.noreply.github.com>
2021-11-11 11:15:24 +0530

    Create musicsic.js

---
+++ filekey/musicsic/musicsic.js
@@ -0,0 +1,5 @@
+if ( 1 > 2 ) {
+    document.getElementById("unikiz-update-musicsic").style.display = "none";
+} else {
+    document.getElementById("unikiz-update-musicsic").style.display = "block";
+}
