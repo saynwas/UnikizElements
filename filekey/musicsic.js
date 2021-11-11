7bcb85d638c90c5babfc4ee991dfd8539871e56f
sayandweep <81100142+sayandweep@users.noreply.github.com>
2021-11-11 11:15:51 +0530

    Delete musicsic.js

--- filekey/musicsic.js
+++
@@ -1,5 +0,0 @@
-if ( 1 < 2 ) {
-    document.getElementById("unikiz-update-musicsic").style.display = "none";
-} else {
-    document.getElementById("unikiz-update-musicsic").style.display = "block";
-}
